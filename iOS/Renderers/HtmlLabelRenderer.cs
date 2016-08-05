82b32f265226cf7371fa8e28b94b9369389fb11e
Blake Davidson <bdavidson@ciedigital.com>
2016-08-05 00:36:13 -0500

    remove wp

--- iOS/Renderers/HtmlLabelRenderer.cs
+++ iOS/Renderers/HtmlLabelRenderer.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using CoreGraphics;
+using Foundation;
 using TechFest.iOS;
 using UIKit;
 using Xamarin.Forms;
@@ -8,25 +9,39 @@ using Xamarin.Forms.Platform.iOS;
 
 namespace TechFest.iOS
 {
-    public class HtmlLabelRenderer : LabelRenderer
+    public class HtmlLabelRenderer : ViewRenderer
     {
-        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
             base.OnElementChanged(e);
+
+            var view = (Label)Element;
+            if (view == null) return;
 
             if (e.NewElement == null)
                 return;
 
-			if (string.IsNullOrEmpty(Control.Text))
+			if (string.IsNullOrEmpty(view.Text))
 				return;
 
-            var text = Control.Text.AsAttributedString(NSDocumentType.HTML);
-            Control.AttributedText = new UITextView { AttributedText = text }.AttributedText;
-            Control.TextColor = e.NewElement.TextColor.ToUIColor();
-            if (!string.IsNullOrEmpty(e.NewElement.FontFamily))
+            UITextView uilabelleftside = new UITextView(new CGRect(0, 0, Element.Width, Element.Height));
+            var text = view.Text.AsAttributedString(NSDocumentType.HTML);
+            uilabelleftside.AttributedText = text;
+            uilabelleftside.Font = UIFont.SystemFontOfSize((float)view.FontSize);
+            uilabelleftside.Editable = false;
+            uilabelleftside.TextColor = view.TextColor.ToUIColor();
+
+            // Setting the data detector types mask to capture all types of link-able data
+            uilabelleftside.DataDetectorTypes = UIDataDetectorType.All;
+            uilabelleftside.BackgroundColor = UIColor.Clear;
+            
+            if (!string.IsNullOrEmpty(view.FontFamily))
             {
-                Control.Font = UIFont.FromName(e.NewElement.FontFamily, (System.nfloat)e.NewElement.FontSize);
+                uilabelleftside.Font = UIFont.FromName(view.FontFamily, (System.nfloat)view.FontSize);
             }
+
+            // overriding Xamarin Forms Label and replace with our native control
+            SetNativeControl(uilabelleftside);
         }
     }
 
